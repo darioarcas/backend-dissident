e978ba8119f7e0a332ddbd5ac78278dc274f3848
darioarcas <darioarcas@hotmail.com>
2026-01-06 15:41:14 -0300

    implementando suscripcion mercadopago

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -85,8 +85,8 @@ router.post('/create_subscription', async (req, res) => {
     await userRef.update({
       suscripcionActiva: true,  // Activamos el campo de suscripción
       suscripcionFechaInicio: admin.firestore.FieldValue.serverTimestamp(),
-      suscripcionFechaVencimiento: admin.firestore.FieldValue.serverTimestamp().toMillis() + 5 * 60 * 1000,  // 5 minutos de prueba
-      // suscripcionFechaVencimiento: admin.firestore.FieldValue.serverTimestamp(),
+      // suscripcionFechaVencimiento: admin.firestore.FieldValue.serverTimestamp().toMillis() + 5 * 60 * 1000,  // 5 minutos de prueba
+      suscripcionFechaVencimiento: admin.firestore.FieldValue.serverTimestamp(),
     });
 
     // Emitir notificación a todos los clientes conectados (opcional)
